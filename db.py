73573a1dfcf64345c15d53bea225ca3617805939
SongZihuan <jimsong2010@163.com>
2022-02-23 00:43:51 +0800

    feat: 更新db程序

--- db.py
+++ db.py
@@ -1,11 +1,8 @@
 import sqlite3
-import threading
 from typing import Optional, Union, List, Tuple, Dict
 import traceback
 
 import pandas
-import pandas as pd
-
 import word
 import re
 
@@ -134,38 +131,33 @@ class WordDatabase(DataBase):
         )''')
         self.wd = word.WordDict()
 
-    def find_word(self, q: str) -> Optional[word.Word]:
-        res = self.search(columns=["id", "word", "part", "english", "chinese", "eg"],
-                          table="Word",
-                          where=f"LOWER(word)='{q.lower()}'")
-        if res is None:
-            res = []
+    def __add_word(self, q: str):
+        r = self.wd.get_requests(q)
+        if not r.is_find:
+            return None
+        ret = None
+        for i in r.res:
+            w = r.res[i]
+            if ret is None:
+                ret = w
+            name = w.name
+            name_lower = name.lower().replace("'", "''")
+            res = self.search(columns=["word"], table="Word", where=f"LOWER(word)='{name_lower}'")
+            if res is not None and len(res) > 0:
+                continue
+            for c in w.comment:
+                comment = r.res[i].comment[c]
+                eg = '@@'.join(comment.eg).replace("'", "''")
+                part = comment.part.replace("'", "''")
+                english = comment.english.replace("'", "''")
+                chinese = comment.chinese.replace("'", "''")
+                self.insert(table='Word',
+                            columns=['word', 'part', 'english', 'chinese', 'eg'],
+                            values=f"'{name_lower}', '{part}', '{english}', '{chinese}', '{eg} '")
+        return ret
 
-        if len(res) <= 0:
-            r = self.wd.get_requests(q)
-            if not r.is_find:
-                return None
-            ret = None
-            for i in r.res:
-                w = r.res[i]
-                if ret is None:
-                    ret = w
-                name = w.name
-                name_lower = name.lower().replace("'", "''")
-                res = self.search(columns=["word"], table="Word", where=f"LOWER(word)='{name_lower}'")
-                if res is not None and len(res) > 0:
-                    continue
-                for c in w.comment:
-                    comment = r.res[i].comment[c]
-                    eg = '@@'.join(comment.eg).replace("'", "''")
-                    part = comment.part.replace("'", "''")
-                    english = comment.english.replace("'", "''")
-                    chinese = comment.chinese.replace("'", "''")
-                    self.insert(table='Word',
-                                columns=['word', 'part', 'english', 'chinese', 'eg'],
-                                values=f"'{name_lower}', '{part}', '{english}', '{chinese}', '{eg} '")
-            return ret
-
+    @staticmethod
+    def __make_word(q: str, res: list):
         w = word.Word(q)
         for i in res:
             c = word.Word.Comment(i[2], i[3], i[4])
@@ -174,11 +166,15 @@ class WordDatabase(DataBase):
             w.add_comment(c)
         return w
 
-    def delete_word(self, q: str):
-        self.delete(table="Word", where=f"LOWER(word)='{q.lower()}'")
-
-    def delete_all(self):
-        self.delete(table="Word")
+    def find_word(self, q: str) -> Optional[word.Word]:
+        res = self.search(columns=["id", "word", "part", "english", "chinese", "eg"],
+                          table="Word",
+                          where=f"LOWER(word)='{q.lower()}'")
+        if res is None:
+            res = []
+        if len(res) <= 0:
+            return self.__add_word(q)
+        return self.__make_word(q, res)
 
     class UpdateResponse:
         def __init__(self):
@@ -192,116 +188,77 @@ class WordDatabase(DataBase):
         def add_error(self, q: str):
             self._error += 1
             self._error_list.append(q)
-            print(f"Error: {q}")
 
         def response(self):
             return self._success, self._error, self._error_list
 
-    def update_from_txt(self, file: str):
+    def import_txt(self, line: str):
         response = self.UpdateResponse()
-        with open(file, encoding='utf-8') as f:
-            for i in f.readlines():
-                word_list = self.word_pattern.findall(i)
-                for w in word_list:
-                    try:
-                        if self.find_word(w) is None:
-                            response.add_error(w)
-                        else:
-                            response.add_success()
-                    except:
-                        response.add_error(w)
-                        traceback.print_exc()
-        return True, response
-
-    def update_from_table(self, file: str, t: str = 'csv'):
-        if t == 'txt':
-            return self.update_from_txt(file)
-        elif t == 'csv':
-            df = pd.read_csv(file, encoding='utf-8', header=None)
-        elif t == 'excel':
-            df = pd.read_excel(file, encoding='utf-8', header=None)
-        elif t == 'json':
-            df = pd.read_json(file, encoding='utf-8')
-        else:
-            return False, []
-
-        print(df)
-        response = self.UpdateResponse()
-        for i in df.itertuples():
-            print(i[1])  # i[0]是序号
+        word_list = self.word_pattern.findall(line)
+        for w in word_list:
             try:
-                if self.find_word(str(i[1])) is None:
-                    response.add_error(str(i[1]))
+                if self.find_word(w) is None:
+                    response.add_error(w)
                 else:
                     response.add_success()
-            except:
-                response.add_error(str(i[1]))
+            except Exception as e:
+                response.add_error(w)
                 traceback.print_exc()
         return True, response
 
-    def export_as_txt(self, file: str):
-        res = self.search(columns=["word"],
-                          table="Word",
-                          order_by=[('box', 'DESC')])
-        if res is None:
-            return False
-        export = []
-        with open(file + '.txt', encoding='utf-8', mode="w") as f:
-            for i in res:
-                if i[0] in export:
-                    continue
-                f.write(i[0] + '\n')
-                export.append(i[0])
-        return True
+    @staticmethod
+    def eg_to_str(eg_filed: str, max_eg: int):
+        eg = eg_filed.split("@@")
+        eg_str = ""
+        count_eg = 0
+        for e in eg:
+            count_eg += 1
+            if max_eg != -1 and count_eg > max_eg:
+                break
 
-    def export_as_table(self, file: str, t: str = 'excel', max_eg: int = 3):
-        if t == 'txt':
-            return self.export_as_txt(file)
+            ec = e.split("##")
+            if len(ec) == 2:
+                eng, chi = ec
+            else:
+                eng = ec[0]
+                chi = ""
+            if len(eng.replace(" ", "")) == 0:
+                continue
+            eg_str += f"{eng} ({chi})\n"
+        return eg_str
 
+    def export_frame(self, max_eg: int = 3) -> Optional[pandas.DataFrame]:
         res = self.search(columns=["box", "word", "part", "english", "chinese", "eg"],
                           table="Word",
                           order_by=[('box', 'DESC')])
         if res is None:
-            return False
+            return None
+
         df = pandas.DataFrame(columns=["Box", "Word", "Part", "English", "Chinese", "Eg"])
         export = []
+
         for i in res:
             if i[1] in export:
                 continue
             export.append(i[1])
-            eg = i[5].split("@@")
-            eg_str = ""
-            count_eg = 0
-            for e in eg:
-                count_eg += 1
-                if max_eg != -1 and count_eg > max_eg:
-                    break
-
-                ec = e.split("##")
-                if len(ec) == 2:
-                    eng, chi = ec
-                else:
-                    eng = ec[0]
-                    chi = ""
-                if len(eng.replace(" ", "")) == 0:
-                    continue
-                eg_str += f"{eng} ({chi})\n"
+            eg_str = self.eg_to_str(i[5], max_eg)
             df = df.append({"Box": str(i[0]),
                             "Word": str(i[1]),
                             "Part": str(i[2]),
                             "English": str(i[3]),
                             "Chinese": str(i[4]),
                             "Eg": eg_str}, ignore_index=True)
-        if t == 'csv':
-            df.to_csv(file + ".csv", encoding='utf-8')
-        elif t == 'excel':
-            df.to_excel(file + ".xlsx", encoding='utf-8')
-        elif t == 'json':
-            df.to_json(file + ".json", encoding='utf-8')
-        else:
-            return False
+        return df
 
+    def delete_txt(self, line: str):
+        count = 0
+        word_list = self.word_pattern.findall(line)
+        for w in word_list:
+            cur = self.delete(table="Word", where=f"LOWER(word)='{w.lower()}'")
+            if cur[1].rowcount != -1:
+                count += 1
+        return count
 
-if __name__ == '__main__':
-    db = WordDatabase()
-    db.export_as_table("resource/English-Word")
+    def delete_all(self):
+        cur = self.delete(table="Word")
+        return cur[1].rowcount
