1520c6d9875689708b34cd2b957f01f1d75606d7
SongZihuan <jimsong2010@163.com>
2022-02-24 23:46:15 +0800

    feat: 添加配置启动程序

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import configure
 import app as App
+import os
+import logging
+
+
+env_dict = os.environ
+hblog_conf = env_dict.get("HENGLISH_CONF")
+if hblog_conf is not None:
+    logging.info(f"Configure file {hblog_conf}")
+    configure.configure(hblog_conf, encoding="utf-8")
+
 
 app = App.HEnglishFlask(__name__)
-
