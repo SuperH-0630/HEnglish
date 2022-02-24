bc55cb443ab76c54b9b36bb0fe093769e2b2a5ad
SongZihuan <jimsong2010@163.com>
2022-02-25 01:08:36 +0800

    typo: hblog_conf -> henglish_conf

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import logging
 
 
 env_dict = os.environ
-hblog_conf = env_dict.get("HENGLISH_CONF")
-if hblog_conf is not None:
-    logging.info(f"Configure file {hblog_conf}")
-    configure.configure(hblog_conf, encoding="utf-8")
+henglish_conf = env_dict.get("HENGLISH_CONF")
+if henglish_conf is not None:
+    logging.info(f"Configure file {henglish_conf}")
+    configure.configure(henglish_conf, encoding="utf-8")
 
 
 app = App.HEnglishFlask(__name__)
